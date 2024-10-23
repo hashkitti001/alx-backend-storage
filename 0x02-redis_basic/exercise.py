a12418ec0f95175fc8ae13ba44540f16b8a36362
hashkitti001 <iJeddy5.0@gmail.com>
2024-10-23 12:39:06 +0100

    add methods to get string and integer representations of the values in the Redis store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import List, Union
+from typing import List, Union, Callable
 import redis
 from uuid import uuid4
 
@@ -15,5 +15,25 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''A method that takes a data argument and returns a string'''
         key = str(uuid4())
-        self._redis.set(key,data)
+        self._redis.set(key, data)
         return data
+
+    def get(
+        self, key: str,
+        fn: Callable = None
+    ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis caching layer'''
+        gotten = self._redis.get(key)
+        # Keeps original Redis.get behavior by returning 
+        # affected data
+        return fn(gotten) if fn is not None else gotten
+    
+    def get_str(self, key: Union[str, bytes, int, float]):
+        '''Returns the string converted value from the Redis storage'''
+        return self.get(key, lambda x: x.decode('utf-8'))
+    
+    def get_int(self, key: Union[str, bytes, int, float]):
+        '''Returns the int representation of a value stored in Redis store'''
+        return self.get(key, lambda x: int(x))
+    
+    
