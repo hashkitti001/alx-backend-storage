ed0b2433482ec1f704206971e97695e83029b1be
hashkitti001 <iJeddy5.0@gmail.com>
2024-10-24 14:25:52 +0100

    add replay function to print a history of calls of a particular fn

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -32,6 +32,30 @@ def call_history(method: Callable) -> Callable:
     return invoker
 
 
+def replay(fn: Callable) -> None:
+    '''Displays the history of calls in a particular function'''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fn_name)
+    out_key = '{}:outputs'.format(fn_name)
+    fn_call_count = 0
+    if redis_store.exists(fn_name) != 0:
+        fn_call_count = int(redis_store.get(fn.name))
+    print('{} was called {} times:'.format(fn_name, fn_call_count))
+    fn_inputs = redis_store.lrange(in_key, 0, -1)
+    fn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fn_input, fn_output in zip(fn_inputs, fn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fn_name,
+            fn_input.decode('utf-8'),
+            fn_output
+        ))
+
+
 class Cache:
     '''A class to represent an object for storing data
 in a Redis caching layer'''
